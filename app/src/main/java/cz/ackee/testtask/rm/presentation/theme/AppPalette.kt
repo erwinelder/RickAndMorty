8d5c3c27fd930af5354edaf88b0bd5dfade39842
Erwin Elder <erwineldermail@gmail.com>
2025-07-16 17:42:42 +0200

    Update app palette.

--- app/src/main/java/cz/ackee/testtask/rm/presentation/theme/AppPalette.kt
+++ app/src/main/java/cz/ackee/testtask/rm/presentation/theme/AppPalette.kt
@@ -3,8 +3,27 @@ package cz.ackee.testtask.rm.presentation.theme
 import androidx.compose.ui.graphics.Color
 
 sealed class AppPalette(
-
+    private val primary: Color,
+    private val disabled: Color,
+    private val background: Color,
+    private val surface: Color,
+    private val onSurface: Color,
+    private val outline: Color,
 ) {
-    data object Light : AppPalette()
-    data object Dark : AppPalette()
+    data object Light : AppPalette(
+        primary = Color(0, 0, 255),
+        disabled = Color(179, 179, 179),
+        background = Color(244, 244, 249),
+        surface = Color(255, 255, 255),
+        onSurface = Color(0, 0, 0),
+        outline = Color(102, 102, 102),
+    )
+    data object Dark : AppPalette(
+        primary = Color(149, 149, 254),
+        disabled = Color(93, 93, 94),
+        background = Color(24, 24, 25),
+        surface = Color(0, 0, 0, 1),
+        onSurface = Color(255, 255, 255),
+        outline = Color(139, 139, 140),
+    )
 }
