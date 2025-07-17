12bbde389396bb8483977be840c4d2168196c502
Erwin Elder <erwineldermail@gmail.com>
2025-07-17 18:06:22 +0200

    Bug fixes.

--- app/src/main/java/cz/ackee/testtask/rm/character/data/local/model/FavouriteCharacterEntity.kt
+++ app/src/main/java/cz/ackee/testtask/rm/character/data/local/model/FavouriteCharacterEntity.kt
@@ -1,8 +1,10 @@
 package cz.ackee.testtask.rm.character.data.local.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "favourite_character")
 data class FavouriteCharacterEntity(
+    @PrimaryKey
     val id: Int
 )
